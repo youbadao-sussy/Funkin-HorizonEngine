621b57b31cb289a02576dfb5715a4c774ed109e8
youbadao-sussy <youbadao@icloud.com>
2026-02-09 08:52:04 +0900

    buildTools Fix

--- setup/windows.bat
+++ setup/windows.bat
@@ -1,1 +1,28 @@
 @echo off
+color 0a
+cd ..
+@echo on
+echo Installing dependencies...
+echo This might take a few moments depending on your internet speed.
+haxelib install lime 8.3.0
+haxelib install openfl 9.5.0
+haxelib install flixel 6.1.2
+haxelib install flixel-addons 4.0.1
+haxelib install flixel-tools 1.5.1
+haxelib install flixel-animate 1.4.0
+haxelib install hscript-iris 1.1.3
+haxelib install haxeui-core 1.7.0
+haxelib install haxeui-flixel 1.7.0
+haxelib install tjson 1.4.0
+haxelib install hxdiscord_rpc 1.3.0
+haxelib install hxvlc 2.2.5 --skip-dependencies
+haxelib install thx.core 0.44.0
+haxelib install thx.semver 0.2.2
+haxelib install tink_core 2.1.1
+haxelib set lime 8.3.0
+haxelib set openfl 9.5.0
+haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 1906c4a96f6bb6df66562b3f24c62f4c5bba14a7
+haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90
+haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666
+echo Finished!
+pause
