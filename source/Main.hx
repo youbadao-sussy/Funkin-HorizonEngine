c1cb1225d338721895893f616b01cb65a5fe218c
youbadao-sussy <youbadao@icloud.com>
2026-02-14 22:31:05 +0900

    ahhhh.....

--- source/Main.hx
+++ source/Main.hx
@@ -119,18 +119,20 @@ class Main extends Sprite
     #if CRASH_HANDLER
     Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onCrash);
     #end
-    // shader coords fix
-    FlxG.signals.gameResized.add(function(w, h) {
-      if (FlxG.cameras != null)
-      {
-        for (cam in FlxG.cameras.list)
+    /*
+      // shader coords fix
+      FlxG.signals.gameResized.add(function(w, h) {
+        if (FlxG.cameras != null)
         {
-          if (cam != null && cam.filters != null) resetSpriteCache(cam.flashSprite);
+          for (cam in FlxG.cameras.list)
+          {
+            if (cam != null && cam.filters != null) resetSpriteCache(cam.flashSprite);
+          }
         }
-      }
 
-      if (FlxG.game != null) resetSpriteCache(FlxG.game);
-    });
+        if (FlxG.game != null) resetSpriteCache(FlxG.game);
+      });
+     */
   }
 
   static function resetSpriteCache(sprite:Sprite):Void
